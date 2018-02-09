053d678861a2a01f2c587dce136f0ff70d74e9da
Janusz Szubert <theinvalidnonce@gmail.com>
2018-02-09 22:18:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function createPost() {
   const pageTemplateFn = _.template(document.getElementById("page-template").innerHTML);
   const postTemplateFn = _.template(document.getElementById("post-template").innerHTML);
-  const commentTemplateFn = _.template(document.getElementById("comment-template").innerHTML);
+  const commentsTemplateFn = _.template(document.getElementById("comment-template").innerHTML);
   
   let postTitle = document.getElementById("postTitle").value;
   let postAuthor = document.getElementById("postAuthor").value;
