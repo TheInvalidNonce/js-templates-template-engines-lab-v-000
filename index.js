09a5633dd59c55f4e13f641b819caaabf63b9c86
Janusz Szubert <theinvalidnonce@gmail.com>
2018-02-09 22:25:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,12 +18,5 @@ function createPost() {
 }
 
 function postComment() {
-  var commentTemplate = _.template(document.getElementById("comment-template").innerHTML);
 
-  var commentText = document.getElementById("commentText").value;
-  var commenterName = document.getElementById("commenter").value;
-
-  var commentsSection = document.getElementById("comments");
-  
-  commentsSection.innerHTML += commentTemplate({ 'commenter': commenterName, 'comment': commentText });
 }
